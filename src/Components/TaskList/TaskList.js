540f51e0fe838407b982d56fbb6ac5d783032518
SMhamidAzad <hamidazad020@gmail.com>
2022-05-19 06:20:55 +0600

    load user task

--- src/Components/TaskList/TaskList.js
+++ src/Components/TaskList/TaskList.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TaskList = () => {
-    
+    const [tasks, setTasks] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/task')
+            .then(res => res.json())
+            .then(data => setTasks(data))
+    }, [])
     return (
         <div className='container'>
             <h2 className="text-center">Task List</h2>
@@ -15,14 +20,19 @@ const TaskList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
+
+                    {
+                        tasks.map((task,index) =>
+                            <tr>
+                                <th scope="row">{index}</th>
+                                <td>{task.name}</td>
+                                <td>{task.description}</td>
+                                <td>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>)
+                    }
+
                 </tbody>
             </table>
         </div>
